5fd84c862af85bc7da015eaac81824b65901fc6a
visual-alchemy <reynandaeldy@gmail.com>
2025-06-29 14:09:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '🖥️ Server Monitoring',
+  description: 'Created with love',
+  generator: 'mind',
 }
 
 export default function RootLayout({
